3ca5f4b1b492bfad224e623ce17d2915b7b95dc5
removethejuice <diegojesuschavezbotto@gmail.com>
2023-11-11 16:47:08 -0600

    mas exitos

--- src/carousel.js
+++ src/carousel.js
@@ -1,19 +1,34 @@
 import Carousel from 'react-bootstrap/Carousel';
-//import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import './Contactanos.css';
 
 function UncontrolledExample() {
   return (
-    <Carousel>
+    
+    <Carousel className='Carousel'>
+
+<Carousel.Item>
+<img src='\images\img1.webp' className='images2' alt='Not Found'></img>
+        <Carousel.Caption>
+          <h3></h3>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
       <Carousel.Item>
-       <img src='\images\Imagentitulo.jpg' alt='Not Found'></img>
+      <img src='\images\img2.jpg' className='images2' alt='Not Found'></img>
+        <Carousel.Caption>
+          <h3></h3>
+         
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src='\images\img2.jpg' className='images2' alt='Not Found'></img>
+        <Carousel.Caption>
+          <h3></h3>
+          <p>
+            
+          </p>
+        </Carousel.Caption>
       
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src='\images\Imagentitulo.jpg' alt='Not Found'></img>
-       
-      </Carousel.Item>
-      <Carousel.Item>
-      <img src='\images\Imagentitulo.jpg' alt='Not Found'></img>
      
       </Carousel.Item>
     </Carousel>
