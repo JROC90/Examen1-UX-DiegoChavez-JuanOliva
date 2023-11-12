e8ce181d1a3434969ef6cc57a104b95acb7b3fd4
JROC90 <10741313@unitec.edu>
2023-11-11 23:48:44 -0600

    ultimo commit

--- src/forma.js
+++ src/forma.js
@@ -1,17 +1,28 @@
 import './forma.css';
+
 const Forma = () => {
+  const handleClick = () => {
+    const inputCorreo = document.getElementById('inputCorreo').value;
+    if (inputCorreo === '') {
+      alert('Por favor, ingrese su correo electrónico');
+    } else {
+      alert(`Te estaremos enviando nuestra información en breve a ${inputCorreo}`);
+    }
+  };
+
   return (
-    <div className='forma-box' >
+    <div className='forma-box'>
       <div className='forma2'>
-        <label> Correo electronico:&nbsp;&nbsp;&nbsp; </label>
+        <label>Correo electronico:&nbsp;&nbsp;&nbsp;</label>
         <input id="inputCorreo" type='text'></input>
       </div>
       <div className='forma3'>
-        <button className='subscribe-button' onClick={() => alert("Te estaremos enviando nuestra informacion en breve" )}>
+        <button className='subscribe-button' onClick={handleClick}>
           SUBSCRIBASE
         </button>
       </div>
     </div>
   );
 }
+
 export default Forma;
